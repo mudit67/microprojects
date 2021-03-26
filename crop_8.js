fe8c26f5fe228f3e2a4478df3da065a50f425f70
mudit67 <bhardwajmudit006@gmail.com>
2021-03-26 14:01:40 +0530

    Changes in crop_8.js org-directory implementation

--- crop_8.js
+++ crop_8.js
@@ -1,86 +1,98 @@
 const { exec } = require("child_process");
 const fs = require("fs");
-const path =  require('path');
+const path = require('path');
 const argv = (process.argv.splice(2))[0];
 // console.log(argv);
-fs.readdir(argv,(err,files) => {
-    if(err){
-        throw err;
-    }
-    // console.log(files);
-    // var argv = process.argv;
-    // console.log(argv);
-    divide2(argv,files,divide4);
+fs.readdir(argv, (err, files) => {
+  if (err) {
+    throw err;
+  }
+  // console.log(files);
+  // var argv = process.argv;
+  // console.log(argv);
+  divide2(argv, files);
 })
-function divide2(arg,files,callback) {
+function divide2(arg, files) {
   // console.log(arguments);
   makeFolder(arg);
-    files.forEach(file => {
-      var pathOriginal = path.join(arg,file)
-      var pathConverted = path.join(makeFolderName(arg),file);
-      exec(`convert -crop 100%X50% \"${pathOriginal}\" \"${pathConverted}\"`, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-            divide4(`${pathConverted.substring(0,pathConverted.lastIndexOf('.'))}-0${pathConverted.substring(pathConverted.lastIndexOf('.'),pathConverted.length)}`);
-            divide4(`${pathConverted.substring(0,pathConverted.lastIndexOf('.'))}-1${pathConverted.substring(pathConverted.lastIndexOf('.'),pathConverted.length)}`);
-        });
+  files.forEach(file => {
+    var pathOriginal = path.join(arg, file)
+    var pathConverted = path.join(makeFolderName(arg,"c"), file);
+    exec(`convert -crop 100%X50% \"${pathOriginal}\" \"${pathConverted}\"`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      divide4(arg,`${file.substring(0, file.lastIndexOf('.'))}-0${file.substring(file.lastIndexOf('.'), file.length)}`);
+      divide4(arg,`${file.substring(0, file.lastIndexOf('.'))}-1${file.substring(file.lastIndexOf('.'), file.length)}`);
     });
-    // callback(arg);
+  });
+  // callback(arg);
 }
-function divide4(arg){
-  var filePath = path.join(arg);
+function divide4(Path,arg) {
+  var filePath = makeFolderName(Path,"c")
+  var filePath = path.join( filePath ,arg);
   exec(`convert -crop 50%X50% \"${filePath}\" \"${filePath}\"`, (error, stdout, stderr) => {
     if (error) {
       console.log(`error: ${error.message}`);
-        return;
-      }
+      return;
+    }
     if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
+      console.log(`stderr: ${stderr}`);
+      return;
     }
     console.log(`stdout: ${stdout}`);
-    fs.unlinkSync(filePath);
+    // fs.unlinkSync(filePath);
+    var newPath = path.join(makeFolderName(Path,"o"),arg);
+    // console.log(newPath, filePath);
+    fs.rename(filePath,newPath,() => {return});
   });
 
 }
 
-function makeFolder(name){
+function makeFolder(name) {
   // console.log(name);
-  var newFolder = makeFolderName(name);
+  var newFolder = makeFolderName(name, "c");
   try {
-      if (!fs.existsSync(newFolder)) {
-        fs.mkdirSync(newFolder)
-      }
-    } catch (err) {
-      console.error(err)
+    if (!fs.existsSync(newFolder)) {
+      fs.mkdirSync(newFolder)
     }
+  } catch (err) {
+    console.error(err)
+  }
+  newFolder = makeFolderName(name, "o");
+  try {
+    if (!fs.existsSync(newFolder)) {
+      fs.mkdirSync(newFolder)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+
 }
 
-function makeFolderName(name){
+function makeFolderName(name, ch) {
   // console.log(name);
-  if(name.endsWith('/')){
-    return (name.substring(0,name.length-1) + "-converted");
+  if (ch == "c") {
+    if (name.endsWith('/')) {
+      return (name.substring(0, name.length - 1) + "-converted");
+    }
+    else {
+      return (name + "-converted");
+    }
   }
-  else{
-    return (name + "-converted");
+  if (ch == "o") {
+    if (name.endsWith('/')) {
+      return (name.substring(0, name.length - 1) + "-org");
+    }
+    else {
+      return (name + "-org");
+
+    }
   }
 }
-
-// exec("ls -la", (error, stdout, stderr) => {
-//     if (error) {
-//         console.log(`error: ${error.message}`);
-//         return;
-//     }
-//     if (stderr) {
-//         console.log(`stderr: ${stderr}`);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout}`);
-// });
