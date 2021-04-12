46c7e879d1524fc9885d711f8bfbe15fa3020151
Mudit Bhardwaj <bhardwajmudit006@gmail.com>
2021-04-12 19:13:15 +0530

    Changed the script to export the function

--- RecursiveDirectoryRead/index.js
+++ RecursiveDirectoryRead/index.js
@@ -1,37 +1,49 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const dirPath = "./sample/"; //Path to directory which contains all the files,folders and sub-folders
-
-let FilesArr = [];
-// setInterval(() => {
-    FilesArr = returnDaughterFiles(dirPath);
-// },1500);
-
-
-// setTimeout(() => {
-    console.log(FilesArr);
-// },2000);
-
-
-
-function returnDaughterFiles(dirToCheck){ //function to return all the files present in the dirToCheck Directory and all sub-directory
-    let ReturnArr = []; // temporary arr to hold all files in dirToCheck and also in its sub-folders
-    let contents = []; // temporary arr to hold files and folder contained in dirToCheck and not in its sub-folders maxDepth=0
-    try {
-       contents = fs.readdirSync(dirToCheck)
-    }catch(err){
-        console.log(err);
-    }
-    contents.forEach(file => {
-        if(fs.lstatSync( path.join(dirToCheck,file)).isFile()){ //check if "file" is a file or not
-            ReturnArr.push(path.join(dirToCheck,file));
-        }
-        else{
-            ReturnArr.push(...returnDaughterFiles(path.join(dirToCheck,file)));
-        }
-    });
-    return(ReturnArr);
-}
 // Function to return whether the argument (path) refers to a directory or a file
-const checkIfFile = path => fs.lstatSync(path).isFile();
+const checkIfFile = (pathToCheck) =>
+  fs.lstatSync(path.join(pathToCheck)).isFile();
+
+// console.log(returnDaughterFiles("src"));
+function returnDaughterFiles(dirToCheck) {
+  //function to return all the files present in the dirToCheck Directory and all sub-directory
+
+  let ReturnArr = []; // temporary arr to hold all files in dirToCheck and also in its sub-folders
+  let contents = []; // temporary arr to hold files and folder contained in dirToCheck and not in its sub-folders maxDepth=0
+  try {
+    contents = fs.readdirSync(dirToCheck);
+  } catch (err) {
+    console.log(err);
+  }
+  contents.forEach((file) => {
+    if (checkIfFile(path.join(dirToCheck,file))) {
+      //check if "file" is a file or not
+      ReturnArr.push(file);
+    } else {
+      ReturnArr.push(...returnDaughterFilesWithoutPath(dirToCheck,file));
+    }
+  });
+  return ReturnArr;
+}
+
+function returnDaughterFilesWithoutPath(rootDir,dirToCheck) {
+  let ReturnArr = []; // temporary arr to hold all files in dirToCheck and also in its sub-folders
+  let contents = []; // temporary arr to hold files and folder contained in dirToCheck and not in its sub-folders maxDepth=0
+  try {
+    contents = fs.readdirSync(path.join(rootDir,dirToCheck));
+  } catch (err) {
+    console.log(err);
+  }
+  contents.forEach((file) => {
+    if (checkIfFile(path.join(rootDir,dirToCheck,file))) {
+      //check if "file" is a file or not
+      ReturnArr.push(dirToCheck+"/"+file);
+    } else {
+      ReturnArr.push(...returnDaughterFilesWithoutPath( rootDir , (dirToCheck+"/"+file)));
+    }
+  });
+  return ReturnArr;
+}
+
+module.exports = returnDaughterFiles;
